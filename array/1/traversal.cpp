4547b40b0ddf7950138a92ace7bd2700e09ae753
khadiza21 <bibikhadiza474@gmail.com>
2022-08-26 02:25:14 +0600

    traversal array

---
+++ array/1/traversal.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printArray (int array[], int size){
+  for (int i=0; i<size; i++){
+        cout<<array[i]<<" ";
+    }
+}
+
+int main (){
+    int array[50];
+    int size;
+    cin>>size;
+    for (int i=0; i<size; i++){
+        cin>>array[i];
+    }
+
+    cout<<"Given Array : ";
+    printArray(array, size);
+}
