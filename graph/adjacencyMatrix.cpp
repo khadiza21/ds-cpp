eae90ca894e5c03269e3378f2fc722f6e51a8e7a
khadiza21 <bibikhadiza474@gmail.com>
2022-12-27 13:16:56 +0600

    adjacency matrix - graph

---
+++ graph/adjacencyMatrix.cpp
@@ -0,0 +1,27 @@
+/* 
+Graph Representation - Adjacency Matrix
+
+0 ----- 1
+       / \
+      2---3
+ 
+# total nodes  = 4 
+# total edge = 4
+# undirected + unweighted
+
+matrix --> #of nodes x #of nodes
+       --> 4x4
+
+matrix[i][j] = 1
+  if and only if (i -> j)
+
+0 -> 1
+1 -> 0
+1 -> 2
+1 -> 3  
+2 -> 1
+2 -> 3
+3 -> 1
+3 -> 2
+
+ */
